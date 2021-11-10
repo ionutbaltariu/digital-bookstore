86d308c11065f5e7f241705396403b9eab225514
xeno-john <ionut.alexandru.baltariu@gmail.com>
2021-11-10 11:40:41 +0200

    Add mongo image in docker-compose.yml

---
+++ setup/mongo/mongo-init.js
@@ -0,0 +1,25 @@
+db.createUser(
+    {
+        user : "user",
+        pwd  : "pass",
+        roles: [
+            {
+                role: "readWrite",
+                db: "orders"
+            }
+        ]
+    }
+)
+
+db.createUser(
+    {
+        user : "admin",
+        pwd  : "pass",
+        roles: [
+            {
+                role: "dbOwner",
+                db: "orders"
+            }
+        ]
+    }
+)
